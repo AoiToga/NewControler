6ea692024379586e906d1b2f1f966ba00708f950
Nicolas <nicolasrodrigues7543@gmail.com>
2022-07-19 18:48:16 -0300

    Upando o NewControler para o GitHub

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////// ALL BUTTON //////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let butt = document.getElementById("files");
+let pop = document.body;
+
+let colors = ['#542344', '#808080', '#D8BFAA', '#E2DACC', '#BFD1E5', '#EBF5EE', '#004777', '#a30000', '#ff7700', '#F7A547', '#F7A547', '#00afb5', '#4B3B47', '#6A6262', '#9c9990', '#CFD2B2', '#E0D8DE']
+butt.addEventListener('click', Background)
+
+
+function Background() {
+
+   let colorsIndex = Math.floor(Math.random() * colors.length)
+   pop.style.backgroundColor = colors[colorsIndex]
+
+
+}
+
+
+
+//////////////////////////////////////////////////////////// ON BUTTON ///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let button = document.getElementById("on");
+let body = document.body;
+
+let color = ['#191919', '#E6E8E6']
+button.addEventListener('click', changeBackground)
+
+
+function changeBackground() {
+
+    let colorsIndex = Math.floor(Math.random() * color.length)
+    body.style.backgroundColor = color[colorsIndex]
+
+
+}
